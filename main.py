8eba9feb60be454035563a660eb8b021221bc546
gerald <gerald.hoff@wsu.edu>
2023-03-04 11:52:38 -0800

    created function populate which populates a graph with a list of PopNode objects

--- main.py
+++ main.py
@@ -2,47 +2,35 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 
-# Stores two object
+# Function that takes a graph of empty nodes, and returns a populated graph.
+# Uses relabel_nodes function:
+def populate(graph, popList):
+    nodeList = sorted(graph)  # gets a list of the node of the graph to be written over
+    mapping = dict(zip(nodeList, popList))  # creates a dictionary of empty nodes and population objects
+    newGraph = nx.relabel_nodes(graph, mapping, True)  # copies the population objects to the original graph
+    return newGraph
+
+
+# Stores two populations (each population stores an integer)
 class PopNode:
     def __init__(self, pop1, pop2):
         self.pop1 = pop1
         self.pop2 = pop2
 
-    # removes first attribute and returns it
-    def rem_attribute1(self):
-        temp_pop = self.pop1
-        self.pop1 = None
-        return temp_pop
-
-    # removes second attribute and returns it
-    def rem_attribute2(self):
-        temp_pop = self.pop2
-        self.pop2 = None
-        return temp_pop
-
-    # adds attribute to PopNode if there is space
-    def add_attribute(self, attribute):
-        if self.pop1 is None:
-            self.pop1 = attribute
-        elif self.pop2 is None:
-            self.pop2 = attribute
-
 
 if __name__ == '__main__':
-    popNode1 = PopNode("Werewolves", "Vampires")
-    popNode2 = PopNode("Sharks", "Jets")
 
-    G = nx.Graph()
-    G.add_node(popNode1)
-    G.add_node(popNode2)
+    # create empty graph
+    fstG = nx.petersen_graph()
 
-    # the first attribute of popNode1 'migrates' to popNode2
-    # it replaces the second attribute in popNode2, which is likely not desirable
-    popNode2.pop2 = popNode1.rem_attribute1()
+    # create list of PopNode objects
+    lst = []
+    for i in range(10):
+        lst.append(PopNode(10, 15))
 
-    G.add_edge(popNode1, popNode2)
+    # create new graph, with list of objects over shape of old graph
+    sndG = populate(fstG, lst)
 
-
-
-    nx.draw(G, with_labels=True, font_weight='bold')
+    nx.draw(sndG, with_labels=True, font_weight='bold')
     plt.show()
+
